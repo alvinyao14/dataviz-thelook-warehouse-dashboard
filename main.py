545bf8b02f9ac6e815f5a62880b1de452374b491
alvinyao14 <Alvin0414@gmail.com>
2025-12-09 23:04:22 -0600

    Reorganized main.py tab order.

--- main.py
+++ main.py
@@ -13,11 +13,11 @@ if not df.empty:
     st.title("📦 TheLook Warehouse Fulfillment Dashboard")
     
     # 2. Create the Tab Structure
-    tab1, tab2, tab3, tab4 = st.tabs([
+    tab1, tab2, tab3, tab4= st.tabs([
         "🚨 Fulfillment Exceptions", 
-        "🌍 Delivery Network", 
-        "💸 Revenue Leakage", 
-        "🏚️ Dead Stock"
+        "🌍 Delivery Network",  
+        "🏚️ Dead Stock",
+        "💸 Revenue Leakage"
     ])
 
     # 3. Render Tabs
@@ -28,10 +28,10 @@ if not df.empty:
         tab_network.render_tab(df)
         
     with tab3:
-        tab_revenue.render_tab(df)
-        
-    with tab4:
         tab_dead_stock.render_tab(df)
+
+    with tab4:
+        tab_revenue.render_tab(df)
 
 else:
     st.warning("Please place 'warehouse_data.csv' in the 'data/' folder to begin.")
